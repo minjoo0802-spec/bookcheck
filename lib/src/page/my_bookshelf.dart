823bc3bcf8ac76ba6f9e9b18bbfa2f4e74159bf5
minjoo_82 <minjookim0802@gmail.com>
2023-10-15 19:51:42 +0900

    1015-2

--- lib/src/page/my_bookshelf.dart
+++ lib/src/page/my_bookshelf.dart
@@ -21,11 +21,42 @@ class _MyBookShelfState extends State<MyBookShelf> {
               padding: const EdgeInsets.all(20),
               crossAxisSpacing: 15,
               mainAxisSpacing: 15,
-              crossAxisCount: 2,
+              crossAxisCount: 3,
               childAspectRatio: 1 / 1.3,
               children: <Widget>[
                 Container(
-                  child: Text("11"),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.7),
+                            spreadRadius: 0,
+                            blurRadius: 0,
+                            offset: Offset(0, 3),
+                          )
+                        ]),
+                    child: Column(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              topRight: Radius.circular(10)),
+                          child: Image(
+                            image: AssetImage(
+                                'assets/images/my_bookshelf_image/book_sample.jpg'),
+                            height: 115,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(left: 6),
+                          child: Text("11"),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
               ]),
         ),
