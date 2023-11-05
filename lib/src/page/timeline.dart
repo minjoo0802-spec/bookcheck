17b479cad6d5b64c3ac704bcb47b07961a1e461b
minjoo <minjookim0802@gmail.com>
2023-11-05 16:49:52 +0900

    1105-1

--- lib/src/page/timeline.dart
+++ lib/src/page/timeline.dart
@@ -5,7 +5,10 @@ import 'package:get/get.dart';
 
 class TimeLinePage extends StatelessWidget {
   final ListItemController controller = Get.put(ListItemController());
-  TimeLinePage({super.key});
+  TimeLinePage({super.key}) {
+    controller.fetchPosts();
+    controller.onInit();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,63 +37,82 @@ class TimeLinePage extends StatelessWidget {
             ),
           ),
         ),
-        SizedBox(
-          height: 400,
-          child: Obx(() {
-            if (controller.listItems.isEmpty) {
-              return Center(child: CircularProgressIndicator());
-            } else {
-              return ListView.builder(
-                itemCount: controller.listItems.length,
-                itemBuilder: (context, index) {
-                  final item = controller.listItems[index];
-                  return SizedBox(
-                    height: 100,
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(4),
-                          child: Image.network(
-                              'https://image.aladin.co.kr/product/32575/8/cover500/k642935143_1.jpg'),
+        GestureDetector(
+          // onTap: () {
+          //   controller.fetchPosts();
+          // },
+          child: SizedBox(
+            height: 400,
+            child: Obx(() {
+              if (controller.listItems.isEmpty) {
+                return Center(child: CircularProgressIndicator());
+              } else {
+                return RefreshIndicator(
+                  onRefresh: () => controller.fetchPosts(),
+                  child: ListView.builder(
+                    itemCount: controller.listItems.length,
+                    itemBuilder: (context, index) {
+                      final item = controller.listItems[index];
+                      return SizedBox(
+                        height: 100,
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(4),
+                              child: Image.network(
+                                  'https://image.aladin.co.kr/product/32575/8/cover500/k642935143_1.jpg'),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 5),
+                                    child: Text(
+                                      item.title,
+                                      style: TextStyle(fontSize: 20),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 5),
+                                    child: Text(item.userName),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 8),
+                                    child: Row(
+                                      children: [
+                                        Text('좋아요 수 ${item.like}'),
+                                        SizedBox(width: 100),
+                                        Text('댓글 수 ${item.reple}'),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(top: 5),
-                                child: Text(
-                                  item.title,
-                                  style: TextStyle(fontSize: 20),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 5),
-                                child: Text(item.userName),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 8),
-                                child: Row(
-                                  children: [
-                                    Text('좋아요 수 ${item.like}'),
-                                    SizedBox(width: 100),
-                                    Text('댓글 수 ${item.reple}'),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              );
-            }
-          }),
+                      );
+                    },
+                  ),
+                );
+              }
+            }),
+          ),
         ),
+        // SizedBox(
+        //   height: 50,
+        //   child: Scaffold(
+        //     floatingActionButton: FloatingActionButton(
+        //       onPressed: () {
+        //         controller.fetchPosts();
+        //       },
+        //       child: Icon(Icons.refresh),
+        //     ),
+        //   ),
+        // ),
       ],
     );
   }
