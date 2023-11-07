748a3c3f57d7f19e35749dcabe2bc664db6f146a
minjoo <minjookim0802@gmail.com>
2023-11-07 16:46:39 +0900

    1107-3

--- lib/src/page/book_info.dart
+++ lib/src/page/book_info.dart
@@ -9,7 +9,7 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
 
 class BookInfo extends StatefulWidget {
-  
+  const BookInfo({super.key});
 
 
   @override
