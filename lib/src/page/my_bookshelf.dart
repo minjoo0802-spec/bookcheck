e6a53ef76cad94947d7218a9bf6c56aa21f29ce4
minjoo <minjookim0802@gmail.com>
2023-12-04 20:40:30 +0900

    1204-2

--- lib/src/page/my_bookshelf.dart
+++ lib/src/page/my_bookshelf.dart
@@ -8,11 +8,9 @@ import '../controller/bookshelf_controller.dart';
 import '../model/book_shelf_model.dart';
 import 'book_report_write.dart';
 
-
 late String? id, qrCode;
 
 class MyBookShelf extends StatelessWidget {
-  
   MyBookShelf({Key? key}) : super(key: key);
 
   final AuthController authController = Get.find();
@@ -28,7 +26,6 @@ class MyBookShelf extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       appBar: MainAppBar(),
       body: Obx(() {
         if (authController.isLoggedIn.value) {
@@ -37,11 +34,10 @@ class MyBookShelf extends StatelessWidget {
             print('받은 데이터 띄우기');
             return Container(
               decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage('assets/images/my_bookshelf_image/bookshelf_back.avif')
-                )
-              ),
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(
+                          'assets/images/my_bookshelf_image/bookshelf_back.avif'))),
               child: Column(
                 children: [
                   logOutBtn(),
@@ -49,13 +45,11 @@ class MyBookShelf extends StatelessWidget {
                 ],
               ),
             );
-          } 
-          else {
+          } else {
             //데이터가 로딩 중인 경우
             print('로딩 중...');
             bookShelfController.fetchBooks(id);
             return Center(child: CircularProgressIndicator());
-            
           }
         } else {
           print('isLoggedIn = false');
@@ -81,12 +75,12 @@ class MyBookShelf extends StatelessWidget {
             context: Get.overlayContext!,
             builder: (context) => StatefulBuilder(
               builder: ((context, setState) => AlertDialog(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                contentPadding: EdgeInsets.only(top: 0),
-                content: Text('로그아웃되었습니다.'),
-              )),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    contentPadding: EdgeInsets.only(top: 0),
+                    content: Text('로그아웃되었습니다.'),
+                  )),
             ),
           );
         },
@@ -110,32 +104,35 @@ class MyBookShelf extends StatelessWidget {
 
   Widget myBookShelfMain(BuildContext context) {
     return Expanded(
-          child: RefreshIndicator(
-            onRefresh: () async {
-              await bookShelfController.fetchBooks(id);
-            },
-            child: Obx(() {
-              if (bookShelfController.books.isNotEmpty) {
-                return Padding(
-                  padding: EdgeInsets.all(10),
-                  child: GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      crossAxisSpacing: 15,
-                      mainAxisSpacing: 15,
-                      childAspectRatio: 0.65,
-                    ),
-                    itemCount: bookShelfController.books.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      Book book = bookShelfController.books[index];
-                      return GestureDetector(
-                        onTap: () async {
-                          print(book.book_report);
-                            Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => BookReportWritePage(index: index)));
-                        },
-                        child: Container(
-                          decoration : BoxDecoration(
+      child: RefreshIndicator(
+        onRefresh: () async {
+          await bookShelfController.fetchBooks(id);
+        },
+        child: Obx(() {
+          if (bookShelfController.books.isNotEmpty) {
+            return Padding(
+              padding: EdgeInsets.all(10),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 15,
+                  mainAxisSpacing: 15,
+                  childAspectRatio: 0.65,
+                ),
+                itemCount: bookShelfController.books.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Book book = bookShelfController.books[index];
+                  return GestureDetector(
+                    onTap: () async {
+                      print(book.book_report);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  BookReportWritePage(index: index)));
+                    },
+                    child: Container(
+                        decoration: BoxDecoration(
                             color: Colors.white,
                             borderRadius: BorderRadius.circular(10),
                             boxShadow: [
@@ -146,20 +143,20 @@ class MyBookShelf extends StatelessWidget {
                                 offset: Offset(0, 3),
                               )
                             ]),
-                          child: _buildBookItem(bookShelfController.books[index])),
-                      );
-                    },
-                  ),
-                );
-              } else {
-                return Center(child: CircularProgressIndicator());
-              }
-            }),
-          ),
-        );
-      }
+                        child:
+                            _buildBookItem(bookShelfController.books[index])),
+                  );
+                },
+              ),
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        }),
+      ),
+    );
   }
-
+}
 
 Widget _buildBookItem(Book book) {
   int maxTitleLength = 8;
@@ -171,12 +168,12 @@ Widget _buildBookItem(Book book) {
   return Card(
     elevation: 0,
     child: ListView(
+      //reverse: true,
       scrollDirection: Axis.vertical,
       children: [
         ClipRRect(
           borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(10),
-            topRight: Radius.circular(10)),
+              topLeft: Radius.circular(10), topRight: Radius.circular(10)),
           child: Image.network(
             book.book_cover,
             height: 150,
@@ -197,5 +194,3 @@ Widget _buildBookItem(Book book) {
     ),
   );
 }
-
-
